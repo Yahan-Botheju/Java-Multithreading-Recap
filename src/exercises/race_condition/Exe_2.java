5134756ee6c0c51ac913b85c5b322f4b3e86d5c1
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-10 14:27:41 +0530

    race_condition: second exercise

--- src/exercises/race_condition/Exe_2.java
+++ src/exercises/race_condition/Exe_2.java
@@ -1,4 +1,32 @@
 package exercises.race_condition;
 
 public class Exe_2 {
+    public static void main(String[] args) {
+        //initiate builder object
+        StringBuilder builder = new StringBuilder();
+
+        //create task
+        Runnable insertLetters = () -> {
+            for (int i = 1; i <= 500 ; i++) {
+                builder.append("A");
+            }
+        };
+
+        //set up threads
+        Thread t1 = new Thread(insertLetters);
+        Thread t2 = new Thread(insertLetters);
+
+        //start threads
+        t1.start();
+        t2.start();
+
+        //set to rest main thread
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){}
+
+        //print values
+        System.out.println("Main Thread Woke");
+        System.out.println(builder.length());
+    }
 }
