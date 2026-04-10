fba1c30042d19e260cf4dc48180fda4d22b16f55
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-10 14:30:47 +0530

    join: first exercise

--- src/exercises/join/Exe_1.java
+++ src/exercises/join/Exe_1.java
@@ -1,4 +1,28 @@
 package exercises.join;
 
 public class Exe_1 {
+    public static void main(String[] args) throws InterruptedException {
+
+        //create task
+         Runnable sumNumbers = () -> {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++) {
+                 sum += i;
+            }
+             System.out.println("Total: " + sum);
+        };
+
+         //define worker
+         Thread t1 = new Thread(sumNumbers);
+
+         //start to work
+         t1.start();
+
+         //force main thread to wait until worker complete work
+         t1.join();
+
+         //when worker completed work, main thread works
+        System.out.println("Main thread working..");
+
+    }
 }
