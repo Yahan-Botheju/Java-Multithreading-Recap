3dfc855d6cddee4183c6dda3948d1ad546ca4abc
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-10 14:26:11 +0530

    runnable: thired exercise

--- src/exercises/runnable/Exe_3.java
+++ src/exercises/runnable/Exe_3.java
@@ -1,4 +1,33 @@
 package exercises.runnable;
 
 public class Exe_3 {
+
+    public static void main(String[] args) {
+
+        //task 1
+        Runnable taskHello = () -> {
+            System.out.print("Hello ");
+        };
+
+        //task 2
+        Runnable taskWorld = () -> {
+            System.out.println("World");
+        };
+
+        //workers create
+        Thread t1 = new Thread(taskHello);
+        Thread t2 = new Thread(taskWorld);
+
+        //start workers
+        t1.start();
+        t2.start();
+
+        //main thread to sleep until work done
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){}
+
+        //msg main thread read
+        System.out.println("Main Thread Woke Up..!!");
+    }
 }
