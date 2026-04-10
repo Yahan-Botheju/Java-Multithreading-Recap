5b149e905e9ef5cfa52af9ad66a250641d7c8efa
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-10 14:30:07 +0530

    race_condition/synchronize: fourth exercise

--- src/exercises/synchronize_raceCondition/Exe_4.java
+++ src/exercises/synchronize_raceCondition/Exe_4.java
@@ -1,4 +1,42 @@
 package exercises.synchronize_raceCondition;
 
 public class Exe_4 {
+
+    static int currentAmount = 700;
+    static int accountBalance;
+
+    static synchronized void account(int withdraw){
+        if(currentAmount >= withdraw){
+            currentAmount = currentAmount - withdraw;
+            accountBalance = currentAmount;
+        }else{
+            System.out.println("Not enough money");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Runnable withdraw = () -> {
+            int amount = 500;
+            account(amount);
+        };
+
+        Thread t1 = new Thread(withdraw);
+        Thread t2 = new Thread(withdraw);
+
+        t1.start();
+        t2.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        System.out.println(accountBalance);
+
+    }
+
 }
