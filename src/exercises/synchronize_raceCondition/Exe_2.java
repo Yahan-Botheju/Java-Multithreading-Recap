af2f90efb74e02aa8259555a3060eb0d0366e349
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-10 14:29:40 +0530

    race_condition/synchronize: second exercise

--- src/exercises/synchronize_raceCondition/Exe_2.java
+++ src/exercises/synchronize_raceCondition/Exe_2.java
@@ -1,4 +1,41 @@
 package exercises.synchronize_raceCondition;
 
 public class Exe_2 {
+    static int count = 0;
+
+     public void increase(){
+        synchronized (this){
+            count++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+         Exe_2 classEx2 = new Exe_2();
+
+        //create task
+        Runnable increment = () -> {
+            for (int i = 1; i <= 1000 ; i++) {
+                classEx2.increase();
+            }
+        };
+
+        //assign task for both workers
+        Thread t1 = new Thread(increment);
+        Thread t2 = new Thread(increment);
+
+        //start
+        t1.start();
+        t2.start();
+
+        //main thread to sleep
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){}
+
+        //print count
+        System.out.println("Main thread woke up");
+        System.out.println(count);
+
+    }
 }
