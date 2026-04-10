25e17267f09f67f3ba97ea42a08308c0e6a99706
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-04-10 14:29:51 +0530

    race_condition/synchronize: thired exercise

--- src/exercises/synchronize_raceCondition/Exe_3.java
+++ src/exercises/synchronize_raceCondition/Exe_3.java
@@ -1,4 +1,32 @@
 package exercises.synchronize_raceCondition;
 
 public class Exe_3 {
+    static int availableTickets = 10;
+
+    public synchronized  void bookTicket(){
+        availableTickets--;
+    }
+
+    public static void main(String[] args) {
+        Exe_3 classExe3 = new Exe_3();
+
+        Runnable buyTicket = () -> {
+          classExe3.bookTicket();
+        };
+
+        Thread t1 = new Thread(buyTicket);
+        Thread t2 = new Thread(buyTicket);
+        Thread t3 = new Thread(buyTicket);
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try{
+            Thread.sleep(3000);
+        }catch (InterruptedException e){}
+
+        System.out.println("Main thread woked");
+        System.out.println("Available Ticket" + availableTickets);
+    }
 }
